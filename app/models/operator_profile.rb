2ccb531f12f6371f1a1c705875f4ccfb5907de77
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-22 16:48:48 +0300

    changed proposal controllers and view

--- app/models/operator_profile.rb
+++ app/models/operator_profile.rb
@@ -1,7 +1,10 @@
 class OperatorProfile < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+
+
+  has_many :proposals, dependent: :destroy
   has_many :reviews, through: :proposals
-  has_many :videos
-  has_many :images
+
+  has_many :videos, dependent: :destroy
+  has_many :images, dependent: :destroy
 end
