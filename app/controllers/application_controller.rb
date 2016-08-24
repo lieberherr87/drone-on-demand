21998a6ba60bdf9efa355522e1b460a3db9037b5
Lama Masri <mail@lamanoppel.com>
2016-08-24 16:59:52 +0300

    completed application & video controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :pilot, :avatar, :avatar_cache])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :pilot, :avatar, :avatar_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :pilot, :avatar, :avatar_cache, :video])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :pilot, :avatar, :avatar_cache, :video])
   end
 end
+
 
 
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,74 @@
 class VideosController < ApplicationController
-  def new
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+
+  # GET /post_attachments
+  # GET /post_attachments.json
+  def index
+    @videos = Video.all
   end
 
-  def create
+  # GET /post_attachments/1
+  # GET /post_attachments/1.json
+  def show
   end
+
+  # GET /post_attachments/new
+  def new
+    @video = Video.new
+  end
+
+  # GET /post_attachments/1/edit
+  def edit
+  end
+
+  # POST /post_attachments
+  # POST /post_attachments.json
+  def create
+    @video = Video.new(image_params)
+
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: 'Post attachment was successfully created.' }
+        format.json { render :show, status: :created, location: @video }
+      else
+        format.html { render :new }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /post_attachments/1
+  # PATCH/PUT /post_attachments/1.json
+  def update
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to @video.post, notice: 'Post attachment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @video }
+      else
+        format.html { render :edit }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /post_attachments/1
+  # DELETE /post_attachments/1.json
+  def destroy
+    @video.destroy
+    respond_to do |format|
+      format.html { redirect_to videos_url, notice: 'Post attachment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_video
+      @video = Video.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def video_params
+      params.require(:post_attachment).permit(:video_id, :video)
+    end
 end
