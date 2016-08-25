d071398bf66bd943cd3569bc8dda6faebcbbceb7
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-25 15:04:42 +0300

    distinguished after registration path between user and pilot

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+
   def after_sign_up_path_for(resource)
-    edit_operator_profile_path(current_user.operator_profile) # Or :prefix_to_your_route
+    if current_user.pilot
+      edit_operator_profile_path(current_user.operator_profile) # Or :prefix_to_your_route
+    else
+      root_path
+    end
   end
 end
