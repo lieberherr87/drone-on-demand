35d98397182a9704aba0acb7f5cb938bb038405a
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-25 12:44:42 +0300

    corrected path after signup to edit operator profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    redirect_to edit_operator_profile_path(current_user.operator_profile) # Or :prefix_to_your_route
+    edit_operator_profile_path(current_user.operator_profile) # Or :prefix_to_your_route
   end
 end
