a39510bf3e39db8694a6574121536f91a3e7786f
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-26 12:43:44 +0300

    messaging working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @conversation.messages.new(message_params)
+
     if @message.save
       redirect_to conversation_messages_path(@conversation)
     end
