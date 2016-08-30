a61cf83bdae8971bdb2312495f4cdef542cfb587
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-30 14:08:37 +0300

    updated request mailer to send emails to each pilot via each

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,11 +1,13 @@
 class RequestMailer < ApplicationMailer
 
-  default bcc: User.where(pilot:true).pluck(:email)
+  # default bcc: User.where(pilot:true).pluck(:email)
+  pilots = User.where(pilot:true)
 
   def request_created(request)
     @request = request
     @greeting = "Hi"
-
-    mail(subject: "Clients need your help: A new request has been created!")
+    pilots.each do |pilot|
+      mail(to: pilot.email, subject: "Clients need your help: A new request has been created!")
+    end
   end
 end
