bbb2f07a2c3eb01a07bb742e3564b858ed71e75d
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-30 14:18:48 +0300

    changed smtp domain to heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'droneit.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
