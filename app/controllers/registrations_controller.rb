18ad5f3c7e4c8ccb4d5471e6d991c6b7f724ae52
Stefan Lieberherr <lieberherr87@gmail.com>
2016-09-01 17:58:45 +0300

    replaced current user with resource

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    if current_user.pilot
+    if resource.pilot
       edit_operator_profiles_path # Or :prefix_to_your_route
     else
       root_path
